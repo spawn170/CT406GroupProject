61793483cab0a7c42578357508eecb230492ac99
I Harley <ijlharley@gmail.com>
2013-04-27 17:48:20 -0400

    Added the "Stops Page"
    Added the stop page which receives the selected stop as a "Get" request
    and displays the listings for it.

---
+++ rentals/stopPage/stopPage.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+</head>
+
+<body>
+<?
+	// gets the name of the stop the user clicked on
+	$stopName = $_GET['stop'];
+	// $lineColor = $_GET['color'];
+	
+	echo "<h3> Listings for ".$stopName."</h3>";
+	
+	$link = mysql_connect('localhost', 'ijharley', 'ct406');
+    $db = mysql_select_db("ct406s13", $link);
+	
+    // selects the listings for the stop selected
+	$sql = "SELECT ih_proj4Listing.name FROM ih_proj4Listing ";
+	// uses an inner join to return only the values where the ids match in both tables
+	$sql .= "INNER JOIN ih_proj4Stop ON ih_proj4Listing.stopId = ih_proj4Stop.id ";
+	$sql .= "WHERE ih_proj4Stop.name = '$stopName' ";
+	$sql .= "ORDER BY ih_proj4Listing.name";
+	
+	$result = mysql_query($sql);
+	
+	echo "<ol>";
+	while ($row = mysql_fetch_array($result))
+    {
+     echo "<li><a href>".$row['name']."</a></li>";
+     echo "<br>";
+    }
+	echo "</ol>";
+	
+    mysql_close($link);
+?>
+</body>
+
+</html>
