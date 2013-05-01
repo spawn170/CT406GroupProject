49bf4c4e25a3b2557eea8e58082dcf8dd6a77d82
Nius <nicholas.p.harrell@gmail.com>
2013-04-30 21:11:42 -0400

    Conformity
    Listing.php now conforms to the rest of the site's CSS standards.

--- rentals/listing.php
+++ rentals/listing.php
@@ -31,32 +31,54 @@
 		$price = substr($price, 0, $i + 1).','.substr($price,$i + 1);
 ?>
 
+<!DOCTYPE html>
+
 <html>
+
 <head>
-<title>Listing near <?=$stopname?></title>
-<link rel="stylesheet" href="listing.css">
+	<link href="styles/rentals.css" rel="stylesheet" />
+	<link href="styles/listing.css" rel="stylesheet" />
+	<title>By Mass Transit - Find Rentals Near D.C.'s Metro Rail Stops</title>
 </head>
+
 <body>
+<div id = "wrapper">
+	<img src="images/bptlogo.jpg" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<img src="images/dcapartment.jpg" />
+	<img src="images/dcloft.jpg" />
+	<img src="images/dcrowhouses.jpg" />
+	
 <?
 	for($i = 0; $i < count($colors); $i ++)
 		echo '<div class="lineD" style="background-color:'.$colors[$i].'"></div>';
 ?>
-<center>
-	<h2>Listing near <?$stopname?>: <?=$name?></h2>
-	<br>
-	<table><tbody>
-		<tr>
-			<td class="left">Address:</td>
-			<td class="right"><?=$addr?></td>
-		</tr><tr>
-			<td class="left">Asking Price:</td>
-			<td class="right">$<?=$price?>.00</td>
-		</tr>
-	</tbody></table>
-	<br>
-	<br>
-	<br>
-	<a href="stop.php?stop=<?=$stop?>">Go Back</a>
-</center>
+	<center>
+		<h2>Listing near <?$stopname?>: <?=$stopname?></h2>
+		<br>
+		<table><tbody>
+			<tr>
+				<td class="left">Name of Residence:</td>
+				<td class="right"><?=$name?></td>
+			<tr></td>
+				<td class="left">Address:</td>
+				<td class="right"><?=$addr?></td>
+			</tr><tr>
+				<td class="left">Asking Price:</td>
+				<td class="right">$<?=$price?>.00</td>
+			</tr>
+		</tbody></table>
+		<br>
+		<br>
+		<br>
+		<a href="stop.php?stop=<?=$stop?>">Go Back</a>
+	</center>
+	<div id="footer">
+			<div id="footerWrapper">
+				<p>&copy;2013. All rights reserved.</p>
+			</div>
+	</div>
+</div>
 </body>
 </html>
