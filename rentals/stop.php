0377c51f03c044000d3e9aa67e8da0d38dd8e1cf
I Harley <ijlharley@gmail.com>
2013-04-30 20:08:17 -0400

    Connected Main Page and Added Styling
    I've connected the main page to the stop page so the links for each stop
    are now working.  I also added the stylesheet to the stop page and, in
    order to test everything, added all of the code currently in the
    "rentals" folder to my leda account.  I made the changes to the main
    page's code on my leda account, so the main page in the respository
    hasn't been changed yet.  I can make the changes if you guys want
    though.

--- rentals/stop.php
+++ rentals/stop.php
@@ -3,38 +3,57 @@
 <html>
 
 <head>
+	<link href="styles/rentals.css" rel="stylesheet">
+	
+	<title>By Mass Transit - Find Rentals Near D.C.'s Metro Rail Stops</title>
 </head>
 
 <body>
-<?
-	// gets the name of the stop the user clicked on
-	$stopName = $_REQUEST['stop'];
-	// $lineColor = $_GET['color'];
+<div id = "wrapper">
+	<img src="images/bptlogo.jpg" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<img src="images/dcapartment.jpg" />
+	<img src="images/dcloft.jpg" />
+	<img src="images/dcrowhouses.jpg" />
 	
-	echo "<h3> Listings for ".$stopName."</h3>";
+	<?
+		// gets the name of the stop the user clicked on
+		$stopName = $_REQUEST['stopName'];
+		$stopID = $_REQUEST['stop'];
+		// $lineColor = $_GET['color'];
+		
+		echo "<h3> Listings for ".$stopName."</h3>";
+		
+		$link = mysql_connect('localhost', 'ijharley', 'ct406');
+		$db = mysql_select_db("ct406s13", $link);
+		
+		// selects the listings for the stop selected
+		$sql = "SELECT ih_proj4Listing.name FROM ih_proj4Listing ";
+		// uses an inner join to return only the values where the ids match in both tables
+		$sql .= "INNER JOIN ih_proj4Stop ON ih_proj4Listing.stopId = ih_proj4Stop.id ";
+		$sql .= "WHERE ih_proj4Stop.id = $stopID ";
+		$sql .= "ORDER BY ih_proj4Listing.name";
+		
+		$result = mysql_query($sql);
+		
+		echo "<ol>";
+		while ($row = mysql_fetch_array($result))
+		{
+		 echo "<li><a href>".$row['name']."</a></li>";
+		 echo "<br>";
+		}
+		echo "</ol>";
+		
+		mysql_close($link);
+	?>
 	
-	$link = mysql_connect('localhost', 'ijharley', 'ct406');
-    $db = mysql_select_db("ct406s13", $link);
-	
-    // selects the listings for the stop selected
-	$sql = "SELECT ih_proj4Listing.name FROM ih_proj4Listing ";
-	// uses an inner join to return only the values where the ids match in both tables
-	$sql .= "INNER JOIN ih_proj4Stop ON ih_proj4Listing.stopId = ih_proj4Stop.id ";
-	$sql .= "WHERE ih_proj4Stop.name = '$stopName' ";
-	$sql .= "ORDER BY ih_proj4Listing.name";
-	
-	$result = mysql_query($sql);
-	
-	echo "<ol>";
-	while ($row = mysql_fetch_array($result))
-    {
-     echo "<li><a href>".$row['name']."</a></li>";
-     echo "<br>";
-    }
-	echo "</ol>";
-	
-    mysql_close($link);
-?>
+	<div id="footer">
+		<div id="footerWrapper">
+			<p>&copy;2013. All rights reserved.</p>
+		</div>
+	</div>
+</div>
 </body>
 
 </html>
